d9dc9e89a3c74876d438b7e855c2f1202571ab11
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-21 06:50:40 +0000

    Deploy Ekhel/frontend-kawal-corona-papua to github.com/Ekhel/frontend-kawal-corona-papua.git:gh-pages

---
+++ precache-manifest.656292ee8c1a52327d3fa44cd764f982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3639df79d24eecfb563331f8490315",
+    "url": "/frontend-kawal-corona-papua/index.html"
+  },
+  {
+    "revision": "855c73455b7fa2848e71",
+    "url": "/frontend-kawal-corona-papua/static/css/main.524d8ad3.chunk.css"
+  },
+  {
+    "revision": "fb72a145390057a8a761",
+    "url": "/frontend-kawal-corona-papua/static/js/2.68519595.chunk.js"
+  },
+  {
+    "revision": "374755f0ec733261f2133b53fa3e8c6c",
+    "url": "/frontend-kawal-corona-papua/static/js/2.68519595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855c73455b7fa2848e71",
+    "url": "/frontend-kawal-corona-papua/static/js/main.4a5d7d9e.chunk.js"
+  },
+  {
+    "revision": "f48ef728004c50fbaa63",
+    "url": "/frontend-kawal-corona-papua/static/js/runtime-main.9643e75d.js"
+  }
+]);
